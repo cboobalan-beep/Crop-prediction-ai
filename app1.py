8b5967c7dc31c7e3d7bc71358bd367c59717a2b9
cboobalan-beep <cboobalan@gmail.com>
2026-05-03 18:41:48 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,6 +12,8 @@ import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+st.write("Files in directory:", os.listdir())
 
 # -------------------------------
 # Page Configuration
